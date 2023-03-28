967b029e87bde3f5fe27634ad65c9d26aa9419c8
Raphael Reitzig <4246780+reitzig@users.noreply.github.com>
2023-03-28 15:13:36 +0200

    fix(jira): Add test.

---
+++ test/test_jira.py
@@ -0,0 +1,15 @@
+from assertpy import assert_that
+
+from label_for_url import determine_label
+
+
+def test_should_label_repository():
+    # Given:
+    url = "https://our-jira.my-org.de/browse/FANCY-77"
+
+    # When:
+    label = determine_label(url)
+
+    # Then:
+    assert_that(label)\
+        .is_equal_to('FANCY-77')
