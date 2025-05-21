ff398c3ea5eafd491eda7cbc4f2a30a6890b46d5
Raphael Reitzig <4246780+reitzig@users.noreply.github.com>
2025-05-21 19:08:00 +0200

    fixup! feat(jira): support jira cloud variant

--- test/test_jira.py
+++ test/test_jira.py
@@ -4,9 +4,17 @@ from assertpy import assert_that
 from label_for_url import determine_label
 
 
-@pytest.mark.parametrize(
-    "url_prefix", ["https://our-jira.my-org.de", "https://my-org.atlassian.net"]
+@pytest.fixture(
+    params=[
+        "https://our-jira.my-org.de",
+        "https://my-org.atlassian.net",
+    ]
 )
+def url_prefix(request: any) -> str:
+    print(type(request))
+    return request.param
+
+
 def test_should_label_project(url_prefix: str) -> None:
     # Given:
     url = f"{url_prefix}/browse/FANCY"
@@ -18,9 +26,6 @@ def test_should_label_project(url_prefix: str) -> None:
     assert_that(label).is_equal_to("FANCY")
 
 
-@pytest.mark.parametrize(
-    "url_prefix", ["https://our-jira.my-org.de", "https://my-org.atlassian.net"]
-)
 def test_should_label_issue(url_prefix: str) -> None:
     # Given:
     url = f"{url_prefix}/browse/FANCY-77"
@@ -32,9 +37,6 @@ def test_should_label_issue(url_prefix: str) -> None:
     assert_that(label).is_equal_to("FANCY-77")
 
 
-@pytest.mark.parametrize(
-    "url_prefix", ["https://our-jira.my-org.de", "https://my-org.atlassian.net"]
-)
 def test_should_label_issue_and_comment(url_prefix: str) -> None:
     # Given:
     url = (
@@ -52,9 +54,6 @@ def test_should_label_issue_and_comment(url_prefix: str) -> None:
     assert_that(label).is_equal_to("FANCY-77.123456")
 
 
-@pytest.mark.parametrize(
-    "url_prefix", ["https://our-jira.my-org.de", "https://my-org.atlassian.net"]
-)
 def test_should_label_issue_and_comment_alternative(url_prefix: str) -> None:
     # Given:
     url = (
@@ -72,11 +71,7 @@ def test_should_label_issue_and_comment_alternative(url_prefix: str) -> None:
     assert_that(label).is_equal_to("FANCY-77.123456")
 
 
-@pytest.mark.parametrize(
-    "url_prefix", ["https://our-jira.my-org.de", "https://my-org.atlassian.net"]
-)
 def test_should_label_servicedesk_issue(url_prefix: str) -> None:
-    # https://our-jira.my-org.de/servicedesk/customer/portal/77/SUPPORT-123
     # Given:
     url = f"{url_prefix}/servicedesk/customer/portal/77/SUPPORT-123"
 
